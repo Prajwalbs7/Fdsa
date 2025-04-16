7f429062927e35a91372bb38bf086425682f91a4
Prajwalbs7 <prajwalshetty0476@gmail.com>
2025-04-16 11:10:44 +0530

    Update program2.py

--- cie3 lab/program2.py
+++ cie3 lab/program2.py
@@ -1,23 +1,22 @@
-class edge
-def __init__(self);
-    self.adj_list = {}
+class Edge:
+    def __init__(self):
+        self.adjacency_list = {}
 
-    def add_edge(self,v,w):
-        if v not in self.adjancency.list
+    def add_edge(self, v, w):
+        if v not in self.adjacency_list:
             self.adjacency_list[v] = []
-    self.adjacecny_list[v].append(w)
+        self.adjacency_list[v].append(w)
 
-    if w not in self.adjacency_list:
-        self.adjacency_list[w] = []
-     self.adjacency_list[w].append(v) 
+        if w not in self.adjacency_list:
+            self.adjacency_list[w] = []
+        self.adjacency_list[w].append(v)
 
     def display(self):
         for vertex in self.adjacency_list:
-            print(f"{vertex}:{self.adjacency_list[vertex]}")
+            print(f"{vertex}: {self.adjacency_list[vertex]}")
 
-        
-   # Example usage
-   e = edge()
+# Example usage:
+e = Edge()
 e.add_edge('A','B')
 e.add_edge('A','C')
 e.add_edge('B','D')
